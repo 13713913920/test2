2d25b8e85efda13b99861c5401c8f8f2ea9fdb4a
keno <24245167@qq.com>
2018-01-08 10:11:10 +0800

    init

---
+++ app/src/main/java/com/keno/test/MyApplication.java
@@ -0,0 +1,17 @@
+package com.keno.test;
+
+import android.app.Application;
+
+import com.blankj.utilcode.util.Utils;
+
+/**
+ * Created by Administrator on 2018/1/5.
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Utils.init(this);
+    }
+}
